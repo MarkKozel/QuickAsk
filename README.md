e772df655fdc9cbf55b44d36b3ed905b38df3764
Mark Kozel <development@markkozel.net>
2020-10-07 11:19:30 -0700

    doc update

--- README.md
+++ README.md
@@ -1,31 +1,52 @@
 # Quick Ask
 
-Based on David Mark Clements's [prompt sync ](https://github.com/heapwolf/prompt-sync), but simplified
 Just need basic user input without any other packages
 
+Simple user input until enter is pressed. backscape will remove last character
+
+Based on David Mark Clements's [prompt sync ](https://github.com/heapwolf/prompt-sync), but simplified
+
+Also referenced [Command Line with ANSI escape codes](https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html)
+
 ## Requires:
+
 - Node 12.x, maybe earlier
 
 ## Usage:
 
+```javascript
+quickAsk = require("../src/QuickAsk");
 
+let reply = quickAsk("type some stuff. Press <enter> when done: ");
+console.log("You typed: " + reply);
+```
+
+results:
+
+```
+type some stuff. Press <enter> when done: klkj;fgjgjjjjg    jjj
+You typed: klkj;fgjgjjjjg    jjj
+```
 
 ## License:
+
 [CC-BY-NC-SA-4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/)
 Attribution-NonCommercial-ShareAlike 4.0 International
 
 ## Miscellany
 
 ### Near Future to-dos
+
 None
 
 ### Version History
-0.0.1 - Inital build-up
+
+0.0.2 - Inital build-up
 
 Added ability to send JavaScript object into constructor, rather than just file. Included automatic write prevention when instantiated this way
 
 <a href="https://www.buymeacoffee.com/MarkKozel" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-blue.png" alt="Buy Me A Coffee" height="41" width="174"></a>
 
-
 ### Historical Context
+
 Global pandemic, US Election a month or so away, US President has Covid, and everyone is very confused
--- src/QuickAsk.js
+++ src/QuickAsk.js
@@ -1,18 +1,27 @@
 const fs = require('fs');
 
 function quickAsk(prompt) {
-  let fd = process.stdin.fd;
   let buf = Buffer.alloc(1);
   let character = '';
   let string = '';
   let cursorPosition = 0;
   let read = '';
+  let echo = 'yes'
+
+  let fd = (process.platform === 'win32') ?
+    process.stdin.fd :
+    fs.openSync('/dev/tty', 'rs');
 
   if (!process.stdin.isRaw) {
     process.stdin.setRawMode && process.stdin.setRawMode(true);
   }
 
-  if (prompt) { console.log(prompt); }
+  if (prompt) {
+    console.log(prompt);
+    process.stdout.moveCursor(prompt.length, -1)
+    // process.stdout.write('\u001b[1A');
+    // process.stdout.write(`\u001b[${prompt.length + (echo == '' ? 0 : cursorPosition)}C`);
+  }
   while (true) {
     read = fs.readSync(fd, buf, 0, 1);
     character = buf[read - 1];
@@ -22,19 +31,27 @@ function quickAsk(prompt) {
       break;
     }
 
-    if (character === 8) { //backspace
+    if (character === 127 || (process.platform == 'win32' && character === 8)) { //backspace
       string = string.slice(0, cursorPosition - 1) + string.slice(cursorPosition);
       cursorPosition--;
-      process.stdout.write('\u001b[2D');
+      process.stdout.moveCursor(-1, 0)
+      // process.stdout.write('\u001b[1D');
+      process.stdout.write(' ');
+      process.stdout.moveCursor(-1, 0)
     } else {
       if ((character < 32) || (character > 126)) {
         continue;
       } else {
-        string = string.slice(0, cursorPosition) + String.fromCharCode(character) + string.slice(cursorPosition);
+        asciiChar = String.fromCharCode(character);
+        string = string.slice(0, cursorPosition) + asciiChar + string.slice(cursorPosition);
         cursorPosition++;
+        process.stdout.write(`\u001b[${prompt.length + (echo == '' ? 0 : cursorPosition)}G`);
+        process.stdout.write('\u001b[s' + asciiChar);
       }
     }
   }
+  process.stdout.write('\u001b[1B'); //newline
+  process.stdout.write(`\u001b[0G`); //column 1
   return string;
 }
 
