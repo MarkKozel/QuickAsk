9d2a385d473b0a23d6cc1139ad219b7f608d31d2
Mark Kozel <development@markkozel.net>
2020-10-07 11:11:37 -0700

    working w backspace

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
 quickAsk = require('../src/QuickAsk');
 
-let reply = quickAsk('types some stuff. Press <enter> when done');
+let reply = quickAsk('types some stuff. Press <enter> when done: ');
 console.log("You typed: " + reply)
